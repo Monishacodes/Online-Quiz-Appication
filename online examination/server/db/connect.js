43bf4f58ef42774908801f6477d68266e7935180
Monisha <31657492+Monishacodes@users.noreply.github.com>
2017-10-10 15:32:34 +0530

    Code

---
+++ online examination/server/db/connect.js
@@ -0,0 +1,4 @@
+var dbConfig = require("./config");
+var mongoose = require("mongoose");
+mongoose.connect(dbConfig.dbURL);
+module.exports=mongoose;
---
+++ online examination/server/db/userschema.js
@@ -0,0 +1,5 @@
+var connection = require("./connect");
+var Schema = connection.Schema;
+var userSchema = new Schema({userid:String,password:String,username:String,roles:[{name:String,rights:[{name:String,descr:String}]}]});
+var User = connection.model("users",userSchema);
+module.exports=User;
